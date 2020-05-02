f49626524a5ede343b1b8c9a3351568f58269e81
Shangaraev Ildar <wigravy@gmail.com>
2020-05-03 03:49:24 +0500

    fix some codestyle

--- src/Game/GameXO.java
+++ src/Game/GameXO.java
@@ -23,25 +23,22 @@ import javax.swing.JPanel;
 
 public class GameXO extends JFrame {
     private final int SIZE = 3;
-    private JButton[][] map = new JButton[3][3];
-    private final ImageIcon DOT_X_IMG = new ImageIcon("src\\Game\\res\\X.png");
-    private final ImageIcon DOT_O_IMG = new ImageIcon("src\\Game\\res\\O.png");
-    private final ImageIcon EMPTY = new ImageIcon("src\\Game\\res\\empty2.jpg");
-    private final ImageIcon ICON = new ImageIcon("src\\Game\\res\\icon.png");
-    private int playerTurn;
+    private JButton[][] map = new JButton[SIZE][SIZE];
+    private final ImageIcon DOT_X_IMG = new ImageIcon(this.getClass().getResource("res\\X.png"));
+    private final ImageIcon DOT_O_IMG = new ImageIcon(this.getClass().getResource("res\\O.png"));
+    private final ImageIcon EMPTY = new ImageIcon(this.getClass().getResource("res\\empty2.jpg"));
+    private final ImageIcon ICON = new ImageIcon(this.getClass().getResource("res\\icon.png"));
+    private int playerTurn = 0;
     private File soundWin = new File("src\\Game\\res\\WinSound.wav");
     private File soundLose = new File("src\\Game\\res\\LoseSound.wav");
+
 
     private JMenu createFileMenu() {
         JMenu file = new JMenu("Файл");
         JMenuItem newGame = new JMenuItem("Новая игра");
         JMenuItem exit = new JMenuItem("Выход");
-        exit.addActionListener((actionEvent) -> {
-            System.exit(0);
-        });
-        newGame.addActionListener((actionEvent) -> {
-            this.newGame();
-        });
+        exit.addActionListener((actionEvent) -> System.exit(0));
+        newGame.addActionListener((actionEvent) -> this.newGame());
         file.add(newGame);
         file.addSeparator();
         file.add(exit);
@@ -53,7 +50,7 @@ public class GameXO extends JFrame {
         new GameXO();
     }
 
-    public void playSoundOnClick(File sound) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+    private void playSoundOnClick(File sound) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         AudioInputStream inAudio = AudioSystem.getAudioInputStream(sound);
         Clip clip = AudioSystem.getClip();
         clip.open(inAudio);
@@ -62,43 +59,34 @@ public class GameXO extends JFrame {
     }
 
     private JPanel gameField() {
-        JPanel panel = new JPanel(new GridLayout(3, 3));
+        JPanel panel = new JPanel(new GridLayout(SIZE, SIZE));
 
-        for(int i = 0; i < 3; ++i) {
-            for(int j = 0; j < 3; ++j) {
+        for(int i = 0; i < SIZE; ++i) {
+            for(int j = 0; j < SIZE; ++j) {
                 this.map[i][j] = new JButton();
                 this.map[i][j].setIcon(this.EMPTY);
                 JButton currentCell = this.map[i][j];
                 currentCell.addActionListener((ActionEvent) -> {
                     currentCell.setDisabledIcon(this.DOT_X_IMG);
                     currentCell.setEnabled(false);
-
                     try {
                         this.checkGame();
-                    } catch (UnsupportedAudioFileException var4) {
-                        var4.printStackTrace();
-                    } catch (IOException var5) {
-                        var5.printStackTrace();
-                    } catch (LineUnavailableException var6) {
-                        var6.printStackTrace();
+                    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                        e.printStackTrace();
                     }
-
                 });
                 panel.add(this.map[i][j]);
             }
         }
-
         return panel;
     }
 
     private void aiTurn() {
         boolean aiwin = true;
         boolean userwin = true;
-        int i;
-        int j;
         if (this.playerTurn > 1) {
-            for(i = 0; i < 3; ++i) {
-                for(j = 0; j < 3; ++j) {
+            for(int i = 0; i < SIZE; ++i) {
+                for(int j = 0; j < SIZE; ++j) {
                     if (this.map[i][j].isEnabled()) {
                         this.map[i][j].setEnabled(false);
                         this.map[i][j].setDisabledIcon(this.DOT_O_IMG);
@@ -107,17 +95,16 @@ public class GameXO extends JFrame {
                         }
 
                         this.map[i][j].setEnabled(true);
-                        this.map[i][j].setDisabledIcon((Icon)null);
+                        this.map[i][j].setDisabledIcon(null);
                     }
                 }
             }
-
             aiwin = false;
         }
 
         if (!aiwin) {
-            for(i = 0; i < 3; ++i) {
-                for(j = 0; j < 3; ++j) {
+            for(int i = 0; i < SIZE; ++i) {
+                for(int j = 0; j < SIZE; ++j) {
                     if (this.map[i][j].isEnabled()) {
                         this.map[i][j].setEnabled(false);
                         this.map[i][j].setDisabledIcon(this.DOT_X_IMG);
@@ -127,79 +114,59 @@ public class GameXO extends JFrame {
                         }
 
                         this.map[i][j].setEnabled(true);
-                        this.map[i][j].setDisabledIcon((Icon)null);
+                        this.map[i][j].setDisabledIcon(null);
                     }
                 }
             }
-
             userwin = false;
         }
 
-        if (!aiwin && !userwin || this.playerTurn < 2) {
-            do {
-                Random rand = new Random();
-                i = rand.nextInt(3);
-                j = rand.nextInt(3);
-            } while(!this.map[i][j].isEnabled());
-
-            this.map[i][j].setDisabledIcon(this.DOT_O_IMG);
-            this.map[i][j].setEnabled(false);
+        if ((!aiwin && !userwin) || this.playerTurn < 2) {
+            int x, y;
+            if (this.map[1][1].isEnabled()) {
+                this.map[1][1].setDisabledIcon(this.DOT_O_IMG);
+                this.map[1][1].setEnabled(false);
+            } else {
+                do {
+                    Random rand = new Random();
+                    x = rand.nextInt(SIZE);
+                    y = rand.nextInt(SIZE);
+                } while (!this.map[x][y].isEnabled());
+                this.map[x][y].setDisabledIcon(this.DOT_O_IMG);
+                this.map[x][y].setEnabled(false);
+            }
         }
-
     }
 
     private boolean checkWin(ImageIcon icon, JButton[][] map) {
-        int countD;
-        int countRD;
-        int countW;
-        for(countD = 0; countD < 3; ++countD) {
-            countRD = 0;
-            countW = 0;
-
-            for(int j = 0; j < 3; ++j) {
-                if (map[countD][j].getDisabledIcon() == icon) {
-                    ++countRD;
-                }
-
-                if (map[j][countD].getDisabledIcon() == icon) {
-                    ++countW;
-                }
-
-                if (countRD == 3 || countW == 3) {
-                    return true;
-                }
+        int countH = 0, countV = 0;
+        for(int i = 0; i < SIZE; ++i) {
+            countH = 0;
+            countV = 0;
+            for(int j = 0; j < SIZE; ++j) {
+                if (map[i][j].getDisabledIcon() == icon) ++countH;
+                if (map[j][i].getDisabledIcon() == icon) ++countV;
+                if (countH == 3 || countV == 3) return true;
             }
         }
 
-        countD = 0;
-        countRD = 0;
-
-        for(countW = 0; countW < 3; ++countW) {
-            if (map[countW][countW].getDisabledIcon() == icon) {
-                ++countD;
-            }
-
-            if (map[countW][3 - countW - 1].getDisabledIcon() == icon) {
-                ++countRD;
-            }
-
-            if (countD == 3 || countRD == 3) {
-                return true;
-            }
+        int countD = 0, countRD = 0;
+        for(int i = 0; i < 3; ++i) {
+            if (map[i][i].getDisabledIcon() == icon) countD++;
+            if (map[i][3 - i - 1].getDisabledIcon() == icon) ++countRD;
+            if (countD == 3 || countRD == 3) return true;
         }
-
         return false;
     }
 
     private boolean isMapFull() {
-        for(int i = 0; i < 3; ++i) {
-            for(int j = 0; j < 3; ++j) {
+        for(int i = 0; i < SIZE; ++i) {
+            for(int j = 0; j < SIZE; ++j) {
                 if (this.map[i][j].isEnabled()) {
                     return false;
                 }
             }
         }
-
         return true;
     }
 
